a0c42d1e2810478ed0de9dd164799d972f6e23f6
vmg <musaravgd@gmail.com>
2024-04-24 18:27:00 +0200

    Styled and position all the form elements. Added event listeners for the input area.

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ document.addEventListener("DOMContentLoaded", function() {
     let confirmPassword = document.querySelector("#confirm-password").value;
 
     let error = document.querySelector("#error");
-
-    if(password !== confirmPassword) {
-        error.style.display = "*Passwords do not match*";
+    if (password==="" && confirmPassword==="") {
+        error.style.display = "*Passwords do not match";
+    }
+    if(password === confirmPassword) {
+        error.style.display = "none";
     }
 })
