b3a824676f47ca8d548295e45f11764867094822
vmg <musaravgd@gmail.com>
2024-04-24 16:30:00 +0200

    Revamped the whole file using newly acquired form skills from freecodecamp. All the elements have been styled save for hover effects.

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    let password = document.querySelector("#password").value;
+    let confirmPassword = document.querySelector("#confirm-password").value;
+
+    let error = document.querySelector("#error");
+
+    if(password !== confirmPassword) {
+        error.style.display = "*Passwords do not match*";
+    }
+})
